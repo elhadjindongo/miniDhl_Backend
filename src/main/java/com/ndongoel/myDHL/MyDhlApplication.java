f2d63fa2ec3a04bc6db2074b3c971cf0115ea5db
El Hadji M. NDONGO <ndongoel@gmail.com>
2021-11-04 16:33:44 +0000

    Project init with (web, spring-data-jpa, h2 db, dev tools & junit test)

---
+++ src/main/java/com/ndongoel/myDHL/MyDhlApplication.java
@@ -0,0 +1,13 @@
+package com.ndongoel.myDHL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDhlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDhlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndongoel/myDHL/MyDhlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndongoel.myDHL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDhlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
